ce0f4b29a80e80a6efdf81e944416745c79cc2a8
Gorka Garcia <gorka.garcia@crealogix.com>
2021-07-01 12:56:58 +0200

    "fixed" virtual scroll

--- source/src/components/virtual-scroll/virtual-scroll.component.ts
+++ source/src/components/virtual-scroll/virtual-scroll.component.ts
@@ -1,16 +1,61 @@
-import { Component, HostListener, Input } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  Input,
+  OnChanges,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-virtual-scroll',
   templateUrl: './virtual-scroll.component.html',
+  styleUrls: ['./virtual-scroll.component.scss'],
 })
-export class VirtualScrollComponent {
+export class VirtualScrollComponent implements OnInit, OnChanges {
   @Input() list: any[];
-  numOfColumns = 6;
+  @Input() rowSizePx: number = 300;
+  itemsPerRow: number;
+  colSize: number;
+  groupList: any[];
 
   @HostListener('window:resize', ['$event'])
   getScreenSize(event?: Event) {
-    // TODO: Set breakpoints
-    this.numOfColumns = window.innerWidth > 767 ? 4 : 6;
+    const newItemPerRow = window.innerWidth > 767 ? 3 : 2;
+    if (newItemPerRow !== this.itemsPerRow) {
+      this.itemsPerRow = newItemPerRow;
+      this.colSize = 12 / this.itemsPerRow;
+      this.reGroupList();
+    }
+  }
+
+  ngOnInit() {
+    this.getScreenSize();
+    this.reGroupList();
+  }
+
+  ngOnChanges(change: any) {
+    if (change.list) {
+      this.reGroupList();
+    }
+  }
+
+  reGroupList() {
+    this.groupList = [];
+    let currentGroup = [];
+    let col = 0;
+
+    for (const element of this.list) {
+      currentGroup.push(element);
+      col++;
+      if (col >= this.itemsPerRow) {
+        this.groupList.push(currentGroup);
+        col = 0;
+        currentGroup = [];
+      }
+    }
+
+    if (currentGroup.length) {
+      this.groupList.push(currentGroup);
+    }
   }
 }
--- source/src/components/virtual-scroll/virtual-scroll.module.ts
+++ source/src/components/virtual-scroll/virtual-scroll.module.ts
@@ -2,12 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { VirtualListIndexPipe } from 'src/pipes/virtual-list-index.pipe';
 import { VirtualScrollComponent } from './virtual-scroll.component';
 
 @NgModule({
   exports: [VirtualScrollComponent],
   imports: [CommonModule, FormsModule, IonicModule],
-  declarations: [VirtualScrollComponent, VirtualListIndexPipe],
+  declarations: [VirtualScrollComponent],
 })
 export class VirtualScrollModule {}
