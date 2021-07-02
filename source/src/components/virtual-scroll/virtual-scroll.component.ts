6612e4fb4e5bb00cc00bafd7b6cc1f78b8db2fb3
Gorka Garcia <gorka.garcia@crealogix.com>
2021-07-02 12:30:05 +0200

    fixed comments

--- source/src/components/virtual-scroll/virtual-scroll.component.ts
+++ source/src/components/virtual-scroll/virtual-scroll.component.ts
@@ -9,9 +9,9 @@ import {
 @Component({
   selector: 'app-virtual-scroll',
   templateUrl: './virtual-scroll.component.html',
-  styleUrls: ['./virtual-scroll.component.scss'],
 })
 export class VirtualScrollComponent implements OnInit, OnChanges {
+  @Input() maxWidth: number = 767;
   @Input() list: any[];
   @Input() rowSizePx: number = 300;
   itemsPerRow: number;
@@ -20,7 +20,7 @@ export class VirtualScrollComponent implements OnInit, OnChanges {
 
   @HostListener('window:resize', ['$event'])
   getScreenSize(event?: Event) {
-    const newItemPerRow = window.innerWidth > 767 ? 3 : 2;
+    const newItemPerRow = this.maxWidth > 767 ? 3 : 2;
     if (newItemPerRow !== this.itemsPerRow) {
       this.itemsPerRow = newItemPerRow;
       this.colSize = 12 / this.itemsPerRow;
