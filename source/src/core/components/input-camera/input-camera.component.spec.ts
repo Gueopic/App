886e9c07e0616304343ce824bd4f2a4c4dc8be0c
ProtocolNebula <ruben@racs.es>
2021-10-18 00:25:58 +0200

    Input camera component started

---
+++ source/src/core/components/input-camera/input-camera.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { InputFileComponent } from '../input-file/input-file.component';
+
+describe('InputFileComponent', () => {
+  let component: InputFileComponent;
+  let fixture: ComponentFixture<InputFileComponent>;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [InputFileComponent],
+      }).compileComponents();
+    })
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InputFileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
