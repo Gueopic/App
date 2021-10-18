0f9311ea98d724fd250d92e1bb706ed9621db654
ProtocolNebula <ruben@racs.es>
2021-10-18 17:31:03 +0200

    Added playground for input camera

--- source/src/app/playground/playground.module.ts
+++ source/src/app/playground/playground.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 import { VirtualScrollModule } from 'src/components/virtual-scroll/virtual-scroll.module';
+import { InputCameraModule } from 'src/core/components/input-camera/input-camera.module';
 import { PlaygroundHomePage } from './playground-home.page';
 import { PlaygroundPageRoutingModule } from './playground-routing.module';
 import { PlaygroundComponentsPage } from './views/components/playground-components.page';
@@ -12,10 +13,13 @@ import { PlaygroundComponentsPage } from './views/components/playground-componen
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     TranslateModule,
     PlaygroundPageRoutingModule,
     VirtualScrollModule,
+
+    InputCameraModule,
   ],
   declarations: [PlaygroundHomePage, PlaygroundComponentsPage],
 })
