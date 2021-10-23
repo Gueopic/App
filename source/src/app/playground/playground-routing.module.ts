af86e69a6b2fa46a24f1ae6f8ab5cd6a87faa028
ProtocolNebula <ruben@racs.es>
2021-10-23 18:36:39 +0200

    Added playground for database

--- source/src/app/playground/playground-routing.module.ts
+++ source/src/app/playground/playground-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlaygroundHomePage } from './playground-home.page';
 import { PlaygroundComponentsPage } from './views/components/playground-components.page';
+import { PlaygroundDatabasePage } from './views/database/playground-database.page';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
       {
         path: 'components',
         component: PlaygroundComponentsPage,
+      },
+      {
+        path: 'database',
+        component: PlaygroundDatabasePage,
       },
     ]
   },
