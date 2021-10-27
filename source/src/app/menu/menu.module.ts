d314e2c264dc49a24efc073a119b9f098dfafbcc
ProtocolNebula <ruben@racs.es>
2021-10-28 01:03:29 +0200

    Added translations to the menu

--- source/src/app/menu/menu.module.ts
+++ source/src/app/menu/menu.module.ts
@@ -2,11 +2,18 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
 import { MenuPageRoutingModule } from './menu-routing.module';
 import { MenuPage } from './menu.page';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, MenuPageRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    TranslateModule,
+    IonicModule,
+    MenuPageRoutingModule,
+  ],
   declarations: [MenuPage],
 })
 export class MenuPageModule {}
