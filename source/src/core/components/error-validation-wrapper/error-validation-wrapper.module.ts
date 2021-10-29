f6e11c53e527ce9e3f7782ff86bb947e8a56948d
ProtocolNebula <ruben@racs.es>
2021-10-30 01:39:47 +0200

    Basic error wrapper component created

---
+++ source/src/core/components/error-validation-wrapper/error-validation-wrapper.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ErrorValidationWrapperComponent } from './error-validation-wrapper.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  declarations: [ErrorValidationWrapperComponent],
+  imports: [CommonModule,
+    IonicModule,
+    TranslateModule,
+  ],
+  exports: [ErrorValidationWrapperComponent],
+})
+export class ErrorValidationWrapperModule {}
