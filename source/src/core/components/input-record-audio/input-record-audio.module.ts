51d38c3a376d7d72767b6e10189e0155c80f086c
ProtocolNebula <ruben@racs.es>
2021-11-07 01:03:23 +0100

    Added translation to input record component

--- source/src/core/components/input-record-audio/input-record-audio.module.ts
+++ source/src/core/components/input-record-audio/input-record-audio.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
 import { InputRecordAudioComponent } from './input-record-audio.component';
 
 @NgModule({
   declarations: [InputRecordAudioComponent],
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, TranslateModule, IonicModule],
   exports: [InputRecordAudioComponent],
 })
 export class InputRecordAudioModule {}
