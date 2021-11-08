c1b596b7710643579c7979d68a93a521b9084a90
ProtocolNebula <ruben@racs.es>
2021-11-08 21:14:54 +0100

    Fixs storage/files models and services

--- source/src/core/utils/file.util.ts
+++ source/src/core/utils/file.util.ts
@@ -4,25 +4,26 @@ import { Filesystem } from '@capacitor/filesystem';
 // https://ionicframework.com/docs/angular/your-first-app/5-adding-mobile
 /**
  * This method get an URI and read the contennt as base64
+ *
  * @param uri HTTP URL (browser) OR file:// path (native)
  * @returns base64 string
  */
 export async function readAsBase64(uri: string): Promise<string> {
   // "hybrid" will detect Cordova or Capacitor
-  // if (Capacitor.isNativePlatform()) {
-  //   // Read the file into base64 format
-  //   const file = await Filesystem.readFile({
-  //     path: uri,
-  //   });
+  if (!Capacitor.isNativePlatform()) {
+    // Read the file into base64 format
+    const file = await Filesystem.readFile({
+      path: uri,
+    });
 
-  //   return file.data;
-  // } else {
+    return file.data;
+  } else {
     // Fetch the photo, read as a blob, then convert to base64 format
     const response = await fetch(uri);
     const blob = await response.blob();
 
     return (await convertBlobToBase64(blob)) as string;
-  // }
+  }
 }
 
 export function convertBlobToBase64(blob: Blob): Promise<string> {
@@ -30,7 +31,7 @@ export function convertBlobToBase64(blob: Blob): Promise<string> {
     const reader = new FileReader();
     reader.onerror = reject;
     reader.onload = () => {
-        resolve(reader.result as string);
+      resolve(reader.result as string);
     };
     reader.readAsDataURL(blob);
   });
