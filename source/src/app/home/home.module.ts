17ebf24365ddd945fc210ebd6eb09de9c8190c74
ProtocolNebula <ruben@racs.es>
2021-11-17 19:20:26 +0100

    Some components refactor and improvements

--- source/src/app/home/home.module.ts
+++ source/src/app/home/home.module.ts
@@ -3,8 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
-import { VirtualScrollModule } from 'src/components/virtual-scroll/virtual-scroll.module';
-
+import { VirtualScrollModule } from 'src/core/components/virtual-scroll/virtual-scroll.module';
 import { HomePageRoutingModule } from './home-routing.module';
 import { HomePage } from './home.page';
 
--- source/src/app/menu/menu.module.ts
+++ source/src/app/menu/menu.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
-import { VirtualScrollModule } from 'src/components/virtual-scroll/virtual-scroll.module';
+import { VirtualScrollModule } from 'src/core/components/virtual-scroll/virtual-scroll.module';
 import { EditObjectModule } from './components/edit-object/edit-object.module';
 import { EditVerbModule } from './components/edit-verb/edit-verb.module';
 import { MenuPageRoutingModule } from './menu-routing.module';
